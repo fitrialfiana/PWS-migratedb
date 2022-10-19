49a1505e45587cbe3dfb014686dc7dc16415d41e
fitrialfiana <70354770+fitrialfiana@users.noreply.github.com>
2022-10-19 23:18:41 +0700

    Update
    Update FIfi

---
+++ migrateedb/src/main/java/com/ti/migrateedb/MigrateedbApplication.java
@@ -0,0 +1,13 @@
+package com.ti.migrateedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrateedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrateedbApplication.class, args);
+	}
+
+}
---
+++ migrateedb/src/test/java/com/ti/migrateedb/MigrateedbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ti.migrateedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrateedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
